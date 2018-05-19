a229b996a45a2f166e8c8ee02ee1f43498c40ce6
Sirwan Afifi <sir1afifi@gmail.com>
2018-05-20 00:03:51 +0430

    added modal component with slots

--- src/script.js
+++ src/script.js
@@ -112,6 +112,31 @@ Vue.component('child', {
     template: '#child-template'
 });
 
+Vue.component('modal', {
+    template: `
+    <div class="modal fade" id="detailsModal" tabindex="-1" role="dialog" aria-labelledby="detailsModalLabel" aria-hidden="false">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="detailsModalLabel">
+                        <slot name="title"></slot>
+                    </h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <slot name="body"></slot>
+                </div>
+                <div class="modal-footer">
+                    <slot name="footer"></slot>
+                </div>
+            </div>
+        </div>
+        </div>
+    `
+});
+
 new Vue({
     el: '#app',
     data() {
